9d47182765ac110c1d6d8cd24d3c36701f1d0042
Bruzzese, Frank <Frank.Bruzzese@Kronos.com>
2018-05-31 15:37:02 -0400

    Fix minor issue with configuration

--- src/components/common.component.js
+++ src/components/common.component.js
@@ -1,6 +1,6 @@
 
-angular.module('common.component', [])
-.service('CommonService', function() {
-    var thing = clone({});
-    console.log("I'm a common service!!");
-});
+define(function(require) {
+
+
+    return 'Hello from a Common Component!';
+})
