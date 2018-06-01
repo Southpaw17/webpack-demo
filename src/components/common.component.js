72e4f9240419fb316c2433753f182bd7de0e5d10
Bruzzese, Frank <Frank.Bruzzese@Kronos.com>
2018-06-01 10:24:11 -0400

    Fixed package.json

--- src/components/common.component.js
+++ src/components/common.component.js
@@ -1,10 +1,3 @@
-
-angular.module('common.component', [])
-.service('CommonService', function() {
-    var thing = clone({});
-    console.log("I'm a common service!!");
-});
-
 define(function(require) {
 
 
